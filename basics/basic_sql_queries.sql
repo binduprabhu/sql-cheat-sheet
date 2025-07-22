5c877017783aeef2017520268ad6f86d56acc079
Bindu Prabhuswamy <68267639+binduprabhu@users.noreply.github.com>
2025-07-22 14:16:46 +0200

    Add files via upload

--- basics/basic_sql_queries.sql
+++ basics/basic_sql_queries.sql
@@ -1,1 +1,17 @@
 
+-- 📌 Basic SQL Structure
+
+-- View all rows
+SELECT * FROM employees;
+
+-- View specific columns
+SELECT employee_id, name, department FROM employees;
+
+-- Remove duplicates
+SELECT DISTINCT department FROM employees;
+
+-- Using simple expressions
+SELECT name, salary, salary * 1.10 AS adjusted_salary FROM employees;
+
+-- Using Aliases
+SELECT name AS employee_name, department AS dept FROM employees;
