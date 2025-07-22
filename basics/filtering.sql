0df8bb1d61394c9f7d2a5b7936fea97fb24b44e6
Bindu Prabhuswamy <68267639+binduprabhu@users.noreply.github.com>
2025-07-22 14:24:08 +0200

    Update filtering.sql

--- basics/filtering.sql
+++ basics/filtering.sql
@@ -1,1 +1,20 @@
+-- 📌 Filtering Data (WHERE, LIKE, IN, BETWEEN)
+
+-- WHERE clause
+SELECT * FROM employees WHERE department = 'Sales';
+
+-- Multiple filters
+SELECT * FROM employees WHERE department = 'Sales' AND salary > 50000;
+
+-- IN clause
+SELECT * FROM employees WHERE department IN ('Sales', 'HR');
+
+-- BETWEEN
+SELECT * FROM employees WHERE salary BETWEEN 50000 AND 80000;
+
+-- LIKE for pattern matching
+SELECT * FROM employees WHERE name LIKE 'A%';
+
+-- NULL value filter
+SELECT * FROM employees WHERE manager_id IS NULL;
 
