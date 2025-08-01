62a289cc708ccb7b69a46e877008dccb9fd0a64b
Bindu Prabhuswamy <68267639+binduprabhu@users.noreply.github.com>
2025-08-02 01:03:33 +0200

    Delete advanced/joins/basic_cte.sql

--- advanced/joins/basic_cte.sql
+++
@@ -1,7 +0,0 @@
--- Basic CTE: Filter employees earning above average salary
-
-WITH high_earners AS (
-    SELECT name, salary FROM employees
-    WHERE salary > (SELECT AVG(salary) FROM employees)
-)
-SELECT * FROM high_earners;
