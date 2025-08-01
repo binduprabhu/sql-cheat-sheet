f9960f544b23f312c408ce2855a951773f804d47
Bindu Prabhuswamy <68267639+binduprabhu@users.noreply.github.com>
2025-08-02 01:03:50 +0200

    Delete advanced/joins/recursive_cte.sql

--- advanced/joins/recursive_cte.sql
+++
@@ -1,8 +0,0 @@
--- Recursive CTE: Generate a sequence of numbers from 1 to 10
-
-WITH RECURSIVE nums AS (
-    SELECT 1 AS number
-    UNION ALL
-    SELECT number + 1 FROM nums WHERE number < 10
-)
-SELECT * FROM nums;
